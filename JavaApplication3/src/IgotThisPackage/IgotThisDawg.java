98b6eae682fb6b2817b7117df82f99a201ce4d8f
T.Rex Northcut <17northcutr@smtexas.org>
2016-09-02 13:52:37 -0500

    Made the file, I got it

---
+++ JavaApplication3/src/IgotThisPackage/IgotThisDawg.java
@@ -0,0 +1,7 @@
+package IgotThisPackage;
+ //@author The Eldritch
+public class IgotThisDawg {
+    public static void main(String[] args) {
+        System.out.println("I got it (dawg)");
+    }
+}
